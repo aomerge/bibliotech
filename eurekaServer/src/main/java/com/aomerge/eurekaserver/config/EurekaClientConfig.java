eddf5227bca5e0e2ec9c030d8cdc5be8c8d0b41c
aomerge <codenode.ortega@gmail.com>
2024-07-17 14:36:07 -0600

    fix new configuration of the  microservices

---
+++ eurekaServer/src/main/java/com/aomerge/eurekaserver/config/EurekaClientConfig.java
@@ -0,0 +1,14 @@
+package com.aomerge.eurekaserver.config;
+
+import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EurekaClientConfig {
+
+    @Bean
+    public AbstractDiscoveryClientOptionalArgs<?> discoveryClientOptionalArgs() {
+        return new AbstractDiscoveryClientOptionalArgs<Object>() {};
+    }
+}
