0292a4e1f87a2ddf74c3f102899fee1ce18e1290
aomerge <codenode.ortega@gmail.com>
2024-07-26 02:19:03 -0600

    add book validatio in the project and mongo config

---
+++ BooksServices/src/main/java/com/aomerge/rentbooks/config/MongoConfig.java
@@ -0,0 +1,13 @@
+package com.aomerge.rentbooks.config;
+
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig { }
+
