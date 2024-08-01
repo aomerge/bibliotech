582c958e9bbc0ec1e9fce1f3895811f35a1d2130
aomerge <codenode.ortega@gmail.com>
2024-07-31 18:46:59 -0600

    binary acces

--- BooksServices/src/main/java/com/aomerge/rentbooks/config/access/AccesBinary.java
+++ BooksServices/src/main/java/com/aomerge/rentbooks/config/access/AccesBinary.java
@@ -1,4 +1,17 @@
 package com.aomerge.rentbooks.config.access;
 
-public class AccesBinary {
+public class AccesBinary extends PermitionBinary {
+    private byte userPermition = 0;
+
+    public void setPermition(byte permition) {
+       userPermition |= permition;
+    }
+
+    public void removePermition(byte permition) {
+        userPermition &= ~permition;
+    }
+
+    public boolean hasPermition(byte permition) {
+        return (userPermition & permition) == permition;
+    }
 }
--- BooksServices/src/main/java/com/aomerge/rentbooks/config/access/PermitionBinary.java
+++ BooksServices/src/main/java/com/aomerge/rentbooks/config/access/PermitionBinary.java
@@ -1,8 +1,12 @@
 package com.aomerge.rentbooks.config.access;
 
 public class PermitionBinary {
-     protected final Byte LECTURE = 0b00000001;
-     protected final Byte WRITE = 0b00000010;
-     protected final Byte DELETE = 0b00000100;
+     protected final Byte READ =    0b0000001;
+     protected final Byte WRITE =   0b0000010;
+     protected final Byte DELETE =  0b0000100;
+     protected final Byte RESERVE = 0b0001000;
+     protected final Byte RETURN =  0b0010000;
+     protected final Byte BORROW =  0b0100000;
+     protected final Byte ADMIN =   0b1000000;
 
 }
