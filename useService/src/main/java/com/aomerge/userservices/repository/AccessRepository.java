6ebc7f559dc7efd52eef6474efe8d142e814f8fb
aomerge <codenode.ortega@gmail.com>
2024-08-03 16:13:28 -0600

    feat: Add findByUserId method to AccessRepository

--- useService/src/main/java/com/aomerge/userservices/repository/AccessRepository.java
+++ useService/src/main/java/com/aomerge/userservices/repository/AccessRepository.java
@@ -4,6 +4,9 @@ import com.aomerge.userservices.models.Access;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccessRepository extends MongoRepository<Access, String> {
+    Optional<Access> findByUserId(String userId);
 }
