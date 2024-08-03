eacb18e640644126a66f42d8766c196530822010
aomerge <codenode.ortega@gmail.com>
2024-08-03 16:12:47 -0600

    feat: Add DBRef annotation to User model

--- useService/src/main/java/com/aomerge/userservices/models/User.java
+++ useService/src/main/java/com/aomerge/userservices/models/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -19,6 +20,8 @@ public class User {
     private String email;
     private String password;
     private String role;
+    @DBRef
+    private Access access;
     @CreatedDate
     private Date createdAt;
     @LastModifiedDate
