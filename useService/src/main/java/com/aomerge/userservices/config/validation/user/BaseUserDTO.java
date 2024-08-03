a05d1e65ce7e7c538fb11d3a82362fb887ca2d1f
aomerge <codenode.ortega@gmail.com>
2024-08-03 16:12:54 -0600

    refactor: Add validation annotations to BaseUserDTO

--- useService/src/main/java/com/aomerge/userservices/config/validation/user/BaseUserDTO.java
+++ useService/src/main/java/com/aomerge/userservices/config/validation/user/BaseUserDTO.java
@@ -1,13 +1,24 @@
 package com.aomerge.userservices.config.validation.user;
 
+import com.aomerge.userservices.config.validation.groups.OnCreate;
+import com.aomerge.userservices.config.validation.groups.OnUpdate;
+import com.aomerge.userservices.models.Access;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class BaseUserDTO {
+    @NotNull(groups = {OnUpdate.class}, message = "El id es requerido")
     private String id;
+    @NotNull(groups = {OnCreate.class}, message = "El nombre es requerido")
     private String name;
+    @NotNull(groups = {OnCreate.class}, message = "El email es requerido")
     private String email;
+    @NotNull(groups = {OnCreate.class}, message = "La contraseña es requerida")
     private String password;
-    private String phone;
-    private String address;
-    private String cpf;
-    private String rg;
-    private String birthDate;
+    @NotNull(groups = {OnCreate.class}, message = "El rol es requerido")
+    private String role;
+    private Access access;
 }
