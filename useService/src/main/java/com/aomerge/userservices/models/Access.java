2e66f7bd9aa6051a34ab5ed9a2f384731efd0da8
aomerge <codenode.ortega@gmail.com>
2024-08-03 16:12:39 -0600

    refactor: Add User reference to Access model

--- useService/src/main/java/com/aomerge/userservices/models/Access.java
+++ useService/src/main/java/com/aomerge/userservices/models/Access.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -17,6 +18,8 @@ public class Access {
     private String id;
     private String branchOffice;
     private byte accesBinary;
+    @DBRef
+    private User userId;
     @CreatedDate
     private Date createdAt;
     @LastModifiedDate
