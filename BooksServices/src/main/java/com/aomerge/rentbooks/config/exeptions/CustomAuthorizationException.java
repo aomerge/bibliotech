6b3670e1e667d3364dc9c096cd0c5608cd819549
aomerge <codenode.ortega@gmail.com>
2024-08-03 07:30:20 -0600

    fix and add CustomAuthorizationException and OnUpdateByBook, fix UserBadRequest.java

--- BooksServices/src/main/java/com/aomerge/rentbooks/config/exeptions/CustomAuthorizationException.java
+++ BooksServices/src/main/java/com/aomerge/rentbooks/config/exeptions/CustomAuthorizationException.java
@@ -1,14 +1,31 @@
 package com.aomerge.rentbooks.config.exeptions;
 
+import com.aomerge.rentbooks.config.validation.books.BaseBookDTO;
+import com.aomerge.rentbooks.config.validation.global.HeaderValidationDTO;
+import jakarta.validation.ConstraintViolation;
+import org.json.JSONObject;
+
+import java.util.Set;
+
 public class CustomAuthorizationException extends RuntimeException{
     private int status;
     private String error;
     private final ErrorDetails errorDetails;
 
-    public CustomAuthorizationException(String message, int status, String error) {
+    public CustomAuthorizationException(int status, Set<ConstraintViolation<HeaderValidationDTO>> violations) {
+        super("Validation failed");
+        this.status = status;
+        JSONObject json = new JSONObject();
+        for (ConstraintViolation<?> violation : violations) {
+            json.put(violation.getPropertyPath().toString(), violation.getMessage());
+        }
+        this.errorDetails = new ErrorDetails(status, json.toString());
+    }
+
+    public CustomAuthorizationException(int status, String message) {
         super(message);
         this.status = status;
-        this.error = error;
+        this.error = message;
         this.errorDetails = new ErrorDetails(status, message);
     }
 
---
+++ BooksServices/src/main/java/com/aomerge/rentbooks/config/validation/category/BaseCaterogyDTO.java
@@ -0,0 +1,57 @@
+package com.aomerge.rentbooks.config.validation.category;
+
+import com.aomerge.rentbooks.config.type.CategoryType;
+import com.aomerge.rentbooks.config.type.CategoryTypeColor;
+import com.aomerge.rentbooks.config.validation.groups.OnCreate;
+import com.aomerge.rentbooks.config.validation.groups.OnUpdate;
+import com.aomerge.rentbooks.config.validation.groups.OnUpdateByBook;
+import com.aomerge.rentbooks.models.Book;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public class BaseCaterogyDTO {
+    @NotNull(groups = OnUpdate.class, message = "Id is required")
+    public String Id;
+
+    @NotNull(groups = OnCreate.class, message = "Category Type Is required")
+    public CategoryType name;
+
+    @NotNull(groups = OnCreate.class, message = "Category Type Is required")
+    public CategoryTypeColor color;
+
+    @NotNull(groups = OnUpdateByBook.class, message = "Book is required")
+    public List<Book> books;
+
+    public @NotNull(groups = OnUpdate.class, message = "Id is required") String getId() {
+        return Id;
+    }
+
+    public void setId(@NotNull(groups = OnUpdate.class, message = "Id is required") String id) {
+        Id = id;
+    }
+
+    public @NotNull(groups = OnCreate.class, message = "Category Type Is required") CategoryType getName() {
+        return name;
+    }
+
+    public void setName(@NotNull(groups = OnCreate.class, message = "Category Type Is required") CategoryType name) {
+        this.name = name;
+    }
+
+    public @NotNull(groups = OnCreate.class, message = "Category Type Is required") CategoryTypeColor getColor() {
+        return color;
+    }
+
+    public void setColor(@NotNull(groups = OnCreate.class, message = "Category Type Is required") CategoryTypeColor color) {
+        this.color = color;
+    }
+
+    public @NotNull(groups = OnUpdateByBook.class, message = "Book is required") List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(@NotNull(groups = OnUpdateByBook.class, message = "Book is required") List<Book> books) {
+        this.books = books;
+    }
+}
