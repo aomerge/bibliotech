f35c0d05d9e92123a731eb2f6f0122c5b94c3de7
aomerge <codenode.ortega@gmail.com>
2024-08-03 18:26:31 -0600

    refactor: Update PermitionBinary class to use INFO instead of RESERVE

--- useService/src/main/java/com/aomerge/userservices/config/access/PermitionBinary.java
+++ useService/src/main/java/com/aomerge/userservices/config/access/PermitionBinary.java
@@ -4,7 +4,7 @@ public class PermitionBinary {
      public final Byte READ =    0b0000001;
      public final Byte WRITE =   0b0000010;
      public final Byte DELETE =  0b0000100;
-     public final Byte RESERVE = 0b0001000;
+     public final Byte INFO   =  0b0001000;
      public final Byte RETURN =  0b0010000;
      public final Byte BORROW =  0b0100000;
      public final Byte ADMIN =   0b1000000;
