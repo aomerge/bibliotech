366696ad79cff7460f38c78fadd9dd5109efce55
aomerge <codenode.ortega@gmail.com>
2024-08-03 18:26:25 -0600

    feat: Add savePermition method to AccesBinary class

--- useService/src/main/java/com/aomerge/userservices/config/access/AccesBinary.java
+++ useService/src/main/java/com/aomerge/userservices/config/access/AccesBinary.java
@@ -8,6 +8,41 @@ public class AccesBinary extends PermitionBinary {
 
     private byte userPermition = 0;
 
+    public Byte savePermition(String permition) {
+        switch (permition.toUpperCase()) {
+            case "USER":
+                setPermition(READ);
+                setPermition(BORROW);
+                setPermition(RETURN);
+                break;
+            case "EMPLOYEE":
+                setPermition(READ);
+                setPermition(WRITE);
+                setPermition(ADMIN);
+                break;
+            case "MANAGER":
+                setPermition(READ);
+                setPermition(WRITE);
+                setPermition(DELETE);
+                setPermition(INFO);
+                setPermition(ADMIN);
+                break;
+            case "ADMIN":
+                setPermition(READ);
+                setPermition(WRITE);
+                setPermition(DELETE);
+                setPermition(INFO);
+                setPermition(BORROW);
+                setPermition(ADMIN);
+                break;
+            default:
+                throw new IllegalArgumentException("Permition not found"+ permition);
+
+
+        }
+        return userPermition;
+    }
+
     public Byte getPermition() {
         return userPermition;
     }
