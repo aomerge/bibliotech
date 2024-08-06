cde00b01dbb3648441c9a021c168329543661353
aomerge <codenode.ortega@gmail.com>
2024-08-06 11:15:26 -0600

    chore: Update EurekaServerApplication to use environment variables for configuration

--- eurekaServer/src/main/java/com/aomerge/eurekaserver/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/aomerge/eurekaserver/EurekaServerApplication.java
@@ -11,10 +11,16 @@ public class EurekaServerApplication {
     protected static final Dotenv dotenv = Dotenv.configure().directory("./").load();
     public static void main(String[] args) throws Exception {
         // config properties the system in envorioment variables
-        System.setProperty("HOST_CONFIG", dotenv.get("HOST_CONFIG"));
-        System.setProperty("PORT_CONFIG", dotenv.get("PORT_CONFIG"));
+        System.setProperty("EUREKA_PORT", dotenv.get("EUREKA_PORT"));
+        System.setProperty("CONFIG_URL", dotenv.get("CONFIG_URL"));
         System.setProperty("ADMIN_USER_CONFIG", dotenv.get("ADMIN_USER_CONFIG"));
         System.setProperty("ADMIN_PASSWORD_CONFIG", dotenv.get("ADMIN_PASSWORD_CONFIG"));
+
+        System.out.println("EUREKA_PORT: " + System.getProperty("EUREKA_PORT"));
+        System.out.println("CONFIG_URI: " + System.getProperty("CONFIG_URL"));
+        System.out.println("ADMIN_USER_CONFIG: " + System.getProperty("ADMIN_USER_CONFIG"));
+        System.out.println("ADMIN_PASSWORD_CONFIG: " + System.getProperty("ADMIN_PASSWORD_CONFIG"));
+
         // inicialing the application
         SpringApplication.run(EurekaServerApplication.class, args);
     }
