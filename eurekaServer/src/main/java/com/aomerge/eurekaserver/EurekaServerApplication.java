84e0d20cd2159a289b2657c46a0d9551d240c040
aomerge <codenode.ortega@gmail.com>
2024-08-07 22:14:25 -0600

    chore: Update EurekaServerApplication to load environment variables from dotenv file

--- eurekaServer/src/main/java/com/aomerge/eurekaserver/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/aomerge/eurekaserver/EurekaServerApplication.java
@@ -12,14 +12,6 @@ public class EurekaServerApplication {
     public static void main(String[] args) throws Exception {
         // config properties the system in envorioment variables
         System.setProperty("EUREKA_PORT", dotenv.get("EUREKA_PORT"));
-        System.setProperty("CONFIG_URL", dotenv.get("CONFIG_URL"));
-        System.setProperty("ADMIN_USER_CONFIG", dotenv.get("ADMIN_USER_CONFIG"));
-        System.setProperty("ADMIN_PASSWORD_CONFIG", dotenv.get("ADMIN_PASSWORD_CONFIG"));
-
-        System.out.println("EUREKA_PORT: " + System.getProperty("EUREKA_PORT"));
-        System.out.println("CONFIG_URI: " + System.getProperty("CONFIG_URL"));
-        System.out.println("ADMIN_USER_CONFIG: " + System.getProperty("ADMIN_USER_CONFIG"));
-        System.out.println("ADMIN_PASSWORD_CONFIG: " + System.getProperty("ADMIN_PASSWORD_CONFIG"));
 
         // inicialing the application
         SpringApplication.run(EurekaServerApplication.class, args);
