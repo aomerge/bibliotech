982d21efde4aefb6c5b1dd58bd1ab77aae286f62
aomerge <codenode.ortega@gmail.com>
2024-08-07 22:14:08 -0600

    chore: Update ConfigServerApplication to load environment variables from dotenv file

--- configServer/src/main/java/com/aomerge/configserver/ConfigServerApplication.java
+++ configServer/src/main/java/com/aomerge/configserver/ConfigServerApplication.java
@@ -11,6 +11,20 @@ public class ConfigServerApplication {
 
 
     public static void main(String[] args) {
+        String activeProfile = System.getProperty("spring.profiles.active", "default");
+
+		Dotenv dotenv;
+		if ("production".equals(activeProfile)) {
+			dotenv = Dotenv.configure().directory("./").load();
+		} else {
+			dotenv = Dotenv.configure().directory("./").load();
+		}
+    
+		System.setProperty("HOST_CONFIG", dotenv.get("HOST_CONFIG"));
+		System.setProperty("PORT_CONFIG", dotenv.get("PORT_CONFIG"));
+		System.setProperty("ADMIN_USER_CONFIG", dotenv.get("ADMIN_USER_CONFIG"));
+		System.setProperty("ADMIN_PASSWORD_CONFIG", dotenv.get("ADMIN_PASSWORD_CONFIG"));
+        
         // run the application
         SpringApplication.run(ConfigServerApplication.class, args);
     }
